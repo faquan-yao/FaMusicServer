0c61270b2571a1611b71a2a35931d44dd685be1a
faquan.yao <faquan.yao@gmail.com>
2021-10-31 18:05:18 +0800

    将项目的开发调试模式切换成上线模式

--- FaMusicServer/settings.py
+++ FaMusicServer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2vpuiw60k_&j+4kwo6gxemp3%pqi39x^4&gx@&#wlgqsqq!m*s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['120.79.203.124']
 
 
 # Application definition
